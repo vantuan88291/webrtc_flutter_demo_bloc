53af771ade873a25eb3118946e8d08670aef1d0f
Tuan Van Le <vantuan88291@gmail.com>
2021-02-25 11:54:54 +0700

    Initial commit

---
+++ lib/app/modules/primary/primary_module.dart
@@ -0,0 +1,17 @@
+import 'package:ecommerce/app/pages/home/home_page.dart';
+import 'package:ecommerce/app/pages/product/product_page.dart';
+import 'package:ecommerce/app/pages/routes.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class PrimaryModule extends ChildModule {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRouter> get routers => [
+    ModularRouter(Modular.initialRoute, child: (_, args) => HomePage()),
+    ModularRouter(Routes.product, child: (_, args) => ProductPage()),
+  ];
+
+  static Inject get to => Inject<PrimaryModule>.of();
+}
